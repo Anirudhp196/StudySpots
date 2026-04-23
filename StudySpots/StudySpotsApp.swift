81612eaf195bb3c145e79c84404ade3393398461
= <anirudhp196@gmail.com>
2026-04-23 17:45:51 -0400

    Initial Commit

---
+++ StudySpots/StudySpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySpotsApp.swift
+//  StudySpots
+//
+//  Created by Anirudh Pasupathy on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct StudySpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
